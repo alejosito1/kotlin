640e652d98bcfb5a6e9b2e9329821a45e310ed47
Sergio Torres <sergio.t94.mez@gmail.com>
2023-09-10 16:31:53 -0500

    Estructuras de Control: when

--- MiPrimerProyecto/src/main/kotlin/Main.kt
+++ MiPrimerProyecto/src/main/kotlin/Main.kt
@@ -12,4 +12,29 @@ fun main(args: Array<String>) {
         "Tienes un nombre corto"
     }
     println(mensaje)
+
+    //Estructura de control: When
+    val nombreColor = "Carmesi"
+    when (nombreColor){
+        "Amarillo" -> println("El amarillo es el color e la alegria")
+        "Rojo", "Carmesi" -> println("El $nombreColor simboliza el calor")
+        else -> println("Error. No tengo informacion del color")
+    }
+
+    val code = 404
+    when(code){
+        //Para rangos.
+        in 200..299 -> println("Todo ha ido bien.")
+        in 400..500 -> println("Algo ha fallado")
+        else -> println("Codigo desconocido, algo ha fallado.")
+    }
+
+    val tallaDeZapatos = 45
+    val mensaje2 = when(tallaDeZapatos){
+        41,43 -> "Tenemos disponible"
+        42,44 -> "Casi no nos quedan"
+        45 -> "Lo siento no tenemos disponibles"
+        else -> "Estos zapatos solo vienen en tallas 41, 42, 43, 44 y 45"
+    }
+    println(mensaje2)
 }
