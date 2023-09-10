7cd034d598b698a96dafaf756dd83cb47307e726
Sergio Torres <sergio.t94.mez@gmail.com>
2023-09-10 16:15:00 -0500

    Estructura de control if

--- MiPrimerProyecto/src/main/kotlin/Main.kt
+++ MiPrimerProyecto/src/main/kotlin/Main.kt
@@ -1,38 +1,15 @@
 //Declarado en tiempo de compilación
 const val PI = 3.1416
 fun main(args: Array<String>) {
-    //Declarado en tiempo de ejecución
-    println("Hello World!")
-    //Variables de lectura y escritura se asignan con var.
-    var dinero = 10
-    println(dinero)
-    dinero = 5
-    println(dinero)
-    //Variables de sobrelectura se asginan con "val", estas no se pueden modificar o reasignar un nuevo valor
-    val nombre = "Maria"
-    println(nombre)
-    println(PI)
-
-    val boolean = true
-    val numeroLargo = 3L
-    val double = 2.7182
-    val float = 1.17f
-
-    val primerValor = 20
-    val segundoValor = 10
-    val tercerValor = primerValor.minus(segundoValor)
-    val tercerValor2 = primerValor - segundoValor
-    println(tercerValor)
-    println(tercerValor2)
-
-    val apellido = "Vetri"
-    val giuseppe = "Giuseppe"
-    //nombreCompleto = nombreCompleto2
-    val nombreCompleto = giuseppe +" "+ apellido
-    //Esta segunda forma nos permite concatenar cadenas de texto de forma facil
-    val nombreCompleto2 = "$giuseppe $apellido"
-    val nombreCompleto3 = "Mi nombre es $giuseppe $apellido"
-    println(nombreCompleto)
-    println(nombreCompleto2)
-    println(nombreCompleto3)
+    val nombre = "Li"
+    //isNotEmpty = compruesta si la variables esta vacia.
+    if (nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}") else println("Error, la variable esta vacia.")
+    val mensaje : String = if (nombre.length > 4){
+        "Tu nombre es largo!"
+    } else if (nombre.isEmpty()){
+        "Nombre esta vacio"
+    } else {
+        "Tienes un nombre corto"
+    }
+    println(mensaje)
 }
