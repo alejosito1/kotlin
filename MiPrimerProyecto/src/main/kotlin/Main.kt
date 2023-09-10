5e4dbd317523520b2b2fa7f28b254a14d70e5aaf
Sergio Torres <sergio.t94.mez@gmail.com>
2023-09-10 13:02:45 -0500

    Kotlin y sus tipos de variables

--- MiPrimerProyecto/src/main/kotlin/Main.kt
+++ MiPrimerProyecto/src/main/kotlin/Main.kt
@@ -12,4 +12,27 @@ fun main(args: Array<String>) {
     val nombre = "Maria"
     println(nombre)
     println(PI)
+
+    val boolean = true
+    val numeroLargo = 3L
+    val double = 2.7182
+    val float = 1.17f
+
+    val primerValor = 20
+    val segundoValor = 10
+    val tercerValor = primerValor.minus(segundoValor)
+    val tercerValor2 = primerValor - segundoValor
+    println(tercerValor)
+    println(tercerValor2)
+
+    val apellido = "Vetri"
+    val giuseppe = "Giuseppe"
+    //nombreCompleto = nombreCompleto2
+    val nombreCompleto = giuseppe +" "+ apellido
+    //Esta segunda forma nos permite concatenar cadenas de texto de forma facil
+    val nombreCompleto2 = "$giuseppe $apellido"
+    val nombreCompleto3 = "Mi nombre es $giuseppe $apellido"
+    println(nombreCompleto)
+    println(nombreCompleto2)
+    println(nombreCompleto3)
 }
