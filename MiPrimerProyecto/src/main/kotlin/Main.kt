aefd34b5cbea0e72e9f308e1665f53fce7b94afb
Sergio Torres <sergio.t94.mez@gmail.com>
2023-10-02 17:03:57 -0500

    Contestando aleatoriamente.

--- MiPrimerProyecto/src/main/kotlin/Main.kt
+++ MiPrimerProyecto/src/main/kotlin/Main.kt
@@ -33,3 +33,51 @@ fun main(args: Array<String>) {
         else -> mostrarError()
     }
 }
+
+fun realizarPregunta(){
+    println("Que pregunta deseas realizar?")
+    readln()
+    println("Asi que esa era tu pregunta... La respuesta a eso es:")
+    val respuestaGenerada = respuestas.keys.random()
+    println(respuestaGenerada)
+}
+
+fun mostrarRespuestas(){
+    println("Seleccion una opcion:")
+    println("1. Revisar todas las respuestas.")
+    println("2. Revisar solo las respuestas afirmativas.")
+    println("3. Revisar solo las respuestas dudosas.")
+    println("4. Revisar solo las respuestas negativas.")
+
+    val opcionIngresada = readLine()
+    when(opcionIngresada){
+        "1" ->  mostrarRespuestasPorTipo()
+        "2" ->  mostrarRespuestasPorTipo(tipoDeRespuesta = RESPUESTA_AFIRMATIVA)
+        "3" ->  mostrarRespuestasPorTipo(tipoDeRespuesta = RESPUESTA_NEGATIVA)
+        "4" ->  mostrarRespuestasPorTipo(tipoDeRespuesta = RESPUESTA_DUDOSA)
+        else -> println("Respuesta no valida.")
+    }
+}
+
+fun mostrarRespuestasPorTipo(tipoDeRespuesta : String = "TODOS"){
+    when(tipoDeRespuesta){
+        "TODOS" -> respuestas.keys.forEach{respuesta -> println(respuesta)}
+
+        RESPUESTA_AFIRMATIVA -> respuestas.filterValues { values -> values == RESPUESTA_AFIRMATIVA }
+            .also { respuestasPositivas -> println(respuestasPositivas.keys) }
+
+        RESPUESTA_NEGATIVA -> respuestas.filterValues { values -> values == RESPUESTA_NEGATIVA }
+            .also { respuestasNegativas -> println(respuestasNegativas.keys) }
+
+        RESPUESTA_DUDOSA -> respuestas.filterValues { values -> values == RESPUESTA_DUDOSA }
+            .also { respuestasDudosas -> println(respuestasDudosas.keys) }
+    }
+}
+
+fun salir(){
+    println("Hasta luego.")
+}
+
+fun mostrarError(){
+    println("Has elegido una opcion no valida, intenta de nuevo.")
+}
